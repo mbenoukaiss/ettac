c85b2b48a685bf71dcf675bd6bcd4fd21226669a
mbenoukaiss <mbenoukaiss@gmail.com>
2026-02-27 21:03:09 +0100

    Build dockerfile and test SSH servers

---
+++ docker/ssh/config.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+if [ -z "$USERNAME" ]; then
+  echo "You must define USERNAME to start SSH server"
+  exit 1
+fi
+
+if [ -z "$PASSWORD" ] && [ -z "$PUBLIC_KEY" ]; then
+  echo "Either define PASSWORD or PUBLIC_KEY to start SSH server"
+  exit 2
+fi
+
+ARGS="-o AddressFamily=inet -o GatewayPorts=yes -o AllowAgentForwarding=yes -o AllowTcpForwarding=yes"
+
+if [ -n "$PERMIT_ROOT_LOGIN" ]; then
+    ARGS="$ARGS -o PermitRootLogin=$PERMIT_ROOT_LOGIN"
+fi
+
+if [ -n "$PASSWORD_AUTHENTICATION" ]; then
+    ARGS="$ARGS -o PasswordAuthentication=$PASSWORD_AUTHENTICATION"
+fi
+
+if [ "$USERNAME" != "root" ]; then
+    HOME=/home/$USERNAME
+    addgroup -S $USERNAME 2>/dev/null
+    adduser -S $USERNAME -G $USERNAME -h $HOME -s /bin/sh
+else
+    HOME=/root
+fi
+
+if [ -n "$PASSWORD" ]; then
+    echo "$USERNAME:$PASSWORD" | chpasswd
+else
+    echo "$USERNAME:!" | chpasswd
+
+    mkdir -p $HOME/.ssh
+    echo "$PUBLIC_KEY" > $HOME/.ssh/authorized_keys
+
+    chown -R $USERNAME:$USERNAME $HOME/.ssh
+    chmod 700 $HOME/.ssh
+    chmod 600 $HOME/.ssh/authorized_keys
+fi
+
+/usr/sbin/sshd -D $ARGS
